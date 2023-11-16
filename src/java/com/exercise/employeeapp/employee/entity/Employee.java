73681697f1fc20efca60fe17c8836ceb6982f5d5
sanmuv <17881536+sanmuv@users.noreply.github.com>
2023-11-16 04:11:18 -0500

    Add files via upload

---
+++ src/java/com/exercise/employeeapp/employee/entity/Employee.java
@@ -0,0 +1,132 @@
+package com.exercise.employeeapp.employee.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+@Table(name = "EMPLOYEE", uniqueConstraints = @UniqueConstraint(columnNames = {"EMPLOYEE_ID"}))
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Employee ID must not be blank")
+    @Column(name = "EMPLOYEE_ID")
+    private String employeeId;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "email is required")
+    @Email
+    private String email;
+
+    @Size(min = 1, message = "At leaast one phone number is required")
+    @ElementCollection
+    private List<@Pattern(regexp = "\\d{10}", message = "Invalid Phone number") String> phoneNumbers;
+
+    @NotNull(message = "Salary is required")
+    private Double salary;
+
+    @NotNull(message ="Date of joining is required")
+    private LocalDate doj;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return employeeId.equals(employee.employeeId) || (firstName.equals(employee.firstName) && lastName.equals(employee.lastName) && email.equals(employee.email));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(List<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getDoj() {
+        return doj;
+    }
+
+    public void setDoj(LocalDate doj) {
+        this.doj = doj;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", employeeId='" + employeeId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumbers=" + phoneNumbers +
+                ", salary=" + salary +
+                ", doj=" + doj +
+                '}';
+    }
+}
