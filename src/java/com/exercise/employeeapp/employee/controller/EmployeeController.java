a68b6f787b46c8fd17c5e7dbfab2c6d77168399e
sanmuv <17881536+sanmuv@users.noreply.github.com>
2023-11-21 12:54:37 -0500

    Removed unused import for DataIntegrityViolation

--- src/java/com/exercise/employeeapp/employee/controller/EmployeeController.java
+++ src/java/com/exercise/employeeapp/employee/controller/EmployeeController.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Size;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
